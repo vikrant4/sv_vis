06d2df892acaebf4018e6a55681effe52045c4c6
Vikrant Yadav <vikrant.y.22@gmail.com>
2019-04-05 14:20:05 +1100

    Basic Flask app

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, send_from_directory
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return send_from_directory(os.path.dirname(__file__), "index.html")
+
+
+@app.route("/api/disposal_data")
+def disposal_data():
+    return send_from_directory(os.path.dirname(__file__), "disposal_data.csv")
+
+
+@app.route("/api/transfer_data")
+def transfer_data():
+    return send_from_directory(os.path.dirname(__file__), "transfer_data.csv")
+
+
+@app.route("/api/transaction_data")
+def transaction_data():
+    return send_from_directory(
+        os.path.dirname(__file__),
+        "transaction_data.csv"
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
