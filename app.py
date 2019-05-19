1d75996606c35c01cc7c332eb6c781da2e8975dd
Vikrant Yadav <vikrant.y.22@gmail.com>
2019-05-19 23:05:54 +1000

    Added filters

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask, send_from_directory, g, jsonify
+from flask import Flask, send_from_directory, g, jsonify, request
 import os
 import sqlite3
 
 
 app = Flask(__name__)
 DATABASE = 'sv.db'
+facility_col_list = [
+    'code',
+    'name',
+    'type',
+    'region',
+    'suburb',
+    'state',
+    'postcode',
+    'active'
+]
+transfer_col_list = [
+    'trno',
+    'in_facility',
+    'datetimein',
+    'weightnett',
+    'item_group',
+    'item_name',
+    'inout',
+    'quantity',
+    'volume',
+    'density',
+    'kg_per_item',
+    'tot_tonnes',
+    'out_facility'
+]
 
 
 def get_db():
+    """Return connection to database"""
     if 'db' not in g:
         g.db = sqlite3.connect(DATABASE)
         g.db.row_factory = sqlite3.Row
@@ -16,6 +42,7 @@ def get_db():
 
 @app.teardown_appcontext
 def close_db(e):
+    """Close connection to database"""
     db = g.pop('db', None)
     if db is not None:
         db.close()
@@ -27,17 +54,50 @@ def main():
 
 @app.route("/")
 def index():
+    """Server index.html"""
     return send_from_directory(
         os.path.dirname(__file__),
         "app_index.html"
     )
 
 
+def query_filter_generator(arguments, col_list, first):
+    """Generate SQL filters for available columns from request arguments"""
+    if arguments.get('transfer_present') == 'on':
+        query_filter = """ WHERE facility.name IN
+        (SELECT in_facility from transfer
+        UNION SELECT out_facility FROM transfer)"""
+    else:
+        query_filter = ''
+    for col in col_list:
+        if arguments.get(col):
+            if arguments.get(col) == 'all':
+                continue
+            elif first and len(query_filter) == 0:
+                query_filter = " WHERE {} = '{}'".format(
+                    col, arguments.get(col))
+            else:
+                query_filter = query_filter + \
+                    " AND {} = '{}'".format(col, arguments.get(col))
+    return query_filter
+
+
 @app.route("/api/facility", methods=['GET'])
 def facility_data():
+    """API to send facility data"""
+    filter_query = query_filter_generator(
+        request.args, facility_col_list, True)
+    print('Filter query', filter_query)
     try:
         db = get_db()
-        result = db.execute('SELECT * FROM facility WHERE facility.name IN(SELECT in_facility FROM transfer UNION SELECT out_facility FROM transfer) ORDER BY latitude DESC').fetchall()
+        result = db.execute(
+            """SELECT * FROM facility /*WHERE
+            facility.name IN(
+            SELECT in_facility FROM transfer
+            UNION
+            SELECT out_facility FROM transfer)*/""" + filter_query +
+            """ ORDER BY latitude DESC""").fetchall()
+        print(jsonify([dict(r) for r in result]))
         return jsonify([dict(r) for r in result])
     except sqlite3.ProgrammingError as e:
         print('SQL ERROR:', e)
@@ -46,12 +106,18 @@ def facility_data():
 
 @app.route("/api/transfer", methods=['GET'])
 def transfer_data():
+    """API tp send transfer data"""
+    filter_query = query_filter_generator(request.args, transfer_col_list, True)
+    print('Filter query', filter_query)
     try:
         db = get_db()
-        result = db.execute('SELECT * from transfer').fetchall()
+        result = db.execute('SELECT * from transfer' + filter_query).fetchall()
         return jsonify([dict(r) for r in result])
     except sqlite3.ProgrammingError as e:
         print('SQL ERROR:', e)
+        return 'Error'
+    except Exception as e:
+        print('ERROR:', e)
         return 'Error'
 
 
